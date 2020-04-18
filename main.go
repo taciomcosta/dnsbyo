6eddafa22492e1c28f97ef204cb5b1bd989f6596
Tacio Costa <taciomcosta@gmail.com>
2020-04-18 18:17:44 -0300

    feat: add reverse lookup

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net"
+	"strings"
 
 	"github.com/google/gopacket"
 	layers "github.com/google/gopacket/layers"
@@ -25,9 +26,9 @@ func readRequestFromBuffer(buff []byte) *layers.DNS {
 	return req
 }
 
-func handle(u *net.UDPConn, clientAddr net.Addr, request *layers.DNS) {
-	transformRequestIntoResponse(request)
-	u.WriteTo(serialize(request), clientAddr)
+func handle(u *net.UDPConn, clientAddr net.Addr, query *layers.DNS) {
+	transformQueryIntoResponse(query)
+	u.WriteTo(serialize(query), clientAddr)
 }
 
 func serialize(req *layers.DNS) []byte {
@@ -36,27 +37,37 @@ func serialize(req *layers.DNS) []byte {
 	return buff.Bytes()
 }
 
-func transformRequestIntoResponse(request *layers.DNS) *layers.DNS {
-	request.AA = true
-	request.Answers = createAnswers(request.Questions)
-	request.ANCount = uint16(len(request.Answers))
-	request.ResponseCode = responseCode(request)
-	request.OpCode = layers.DNSOpCodeNotify
-	request.QR = true
-	return request
+func transformQueryIntoResponse(query *layers.DNS) *layers.DNS {
+	query.AA = true
+	query.Answers = createAnswers(query)
+	query.ANCount = uint16(len(query.Answers))
+	query.ResponseCode = responseCode(query)
+	query.QR = true
+	return query
 }
 
-func createAnswers(questions []layers.DNSQuestion) []layers.DNSResourceRecord {
+func createAnswers(req *layers.DNS) []layers.DNSResourceRecord {
 	var answers []layers.DNSResourceRecord
-	for _, q := range questions {
-		if rr, err := createRR(q.Name); err == nil {
+	for i, _ := range req.Questions {
+		if rr, err := createRR(req, i); err == nil {
 			answers = append(answers, rr)
 		}
 	}
 	return answers
 }
 
-func createRR(hostname []byte) (layers.DNSResourceRecord, error) {
+func createRR(query *layers.DNS, i int) (layers.DNSResourceRecord, error) {
+	if isReverseLookup(query.Questions[0].Name) {
+		return reverseRR(query.Questions[0].Name)
+	}
+	return standardRR(query.Questions[0].Name)
+}
+
+func isReverseLookup(name []byte) bool {
+	return strings.HasSuffix(string(name), ".in-addr.arpa")
+}
+
+func standardRR(hostname []byte) (layers.DNSResourceRecord, error) {
 	ip, err := findIP(hostname)
 	if err != nil {
 		return layers.DNSResourceRecord{}, err
@@ -73,9 +84,28 @@ func existingRR(ip net.IP, hostname []byte) layers.DNSResourceRecord {
 	}
 }
 
-func responseCode(request *layers.DNS) layers.DNSResponseCode {
-	if len(request.Answers) > 0 {
+func responseCode(query *layers.DNS) layers.DNSResponseCode {
+	if len(query.Answers) > 0 {
 		return layers.DNSResponseCodeNoErr
 	}
 	return layers.DNSResponseCodeNXDomain
+}
+
+func reverseRR(ipInAddrArpa []byte) (layers.DNSResourceRecord, error) {
+	ip := parseInAddrArpa(ipInAddrArpa)
+	name, err := findName(ip)
+	if err != nil {
+		return layers.DNSResourceRecord{}, err
+	}
+	return existingRR(ip, name), nil
+}
+
+func parseInAddrArpa(ipInAddrArpa []byte) net.IP {
+	ip := strings.Replace(string(ipInAddrArpa), ".in-addr.arpa", "", 1)
+	octets := strings.Split(ip, ".")
+	if len(octets) != 4 {
+		return nil
+	}
+	parsedIP := net.ParseIP(octets[3] + "." + octets[2] + "." + octets[1] + "." + octets[0])
+	return parsedIP
 }
--- records.go
+++ records.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net"
 )
 
+const recordsFilePath = "records.json"
+
 func findIP(name []byte) (net.IP, error) {
-	records := fromJSON("records.json")
+	records := fromRecordsFile()
 	ip, _ := records[string(name)]
 	parsedIP, _, err := net.ParseCIDR(ip + "/24")
 	return parsedIP, err
 }
 
-func fromJSON(path string) map[string]string {
-	bytes := readFile(path)
+func fromRecordsFile() map[string]string {
+	bytes := readFile(recordsFilePath)
 	records := make(map[string]string)
 	json.Unmarshal(bytes, &records)
 	return records
@@ -26,4 +29,14 @@ func readFile(path string) []byte {
 		return []byte{}
 	}
 	return bytes
+}
+
+func findName(requestedIP net.IP) ([]byte, error) {
+	records := fromRecordsFile()
+	for name, ip := range records {
+		if requestedIP.Equal(net.ParseIP(ip)) {
+			return []byte(name), nil
+		}
+	}
+	return []byte{}, fmt.Errorf("No name found for %s", requestedIP.String())
 }
